57304396f43ff7b0af7a1a21e06c76d0344dbf94
Michael Bezick <mbezick@purdue.edu>
2026-01-12 11:06:53 -0500

    Added neogit plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,46 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css", "clangd"
-  -- 		},
-  -- 	},
-  -- },
+	-- Neogit
+	{
+		"NeogitOrg/neogit",
+		cmd = "Neogit",
+		keys = {
+			{ "<leader>gg", "<cmd>Neogit<CR>", desc = "Show Neogit UI" },
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional (diff integration)
+			"nvim-telescope/telescope.nvim", -- optional (picker integration)
+		},
+		config = function()
+			require("neogit").setup({
+				integrations = {
+					diffview = true,
+					telescope = true,
+				},
+			})
+		end,
+	},
+	
+	-- {
+	--   "nvim-treesitter/nvim-treesitter",
+	--   opts = {
+	--     ensure_installed = {
+	--       "vim", "lua", "vimdoc",
+	--       "html", "css", "clangd"
+	--     },
+	--   },
+	-- },
 }
